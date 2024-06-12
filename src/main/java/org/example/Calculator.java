2f7bc542e0f3141983342f9c6b5c1c98961a740c
nameGeorge <geomel@yandex.ru>
2024-06-12 23:06:49 +0300

    питаюсь сделать метод из калькулятора

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -3,9 +3,7 @@ package org.example;
 public class Calculator {
     public static String runCalculator() {
         int first = Input.scan();
-
         char operator = Input.readChar();
-
         int second = Input.scan();
 
         String result;
