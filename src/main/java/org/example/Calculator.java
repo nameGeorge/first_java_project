e0e3a9205fdbbb86398e54628110ad7d09ec0096
nameGeorge <geomel@yandex.ru>
2024-06-12 23:10:06 +0300

    питаюсь сделать метод из калькулятора

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -3,30 +3,18 @@ package org.example;
 public class Calculator {
     public static String runCalculator() {
         int first = Input.scan();
+
         char operator = Input.readChar();
+
         int second = Input.scan();
 
-        String result;
-        switch (operator) {
-            case '+':
-                 result = Integer.toString(first + second);
 
-                break;
-            case '-':
-                result = Integer.toString(first - second);
-
-                break;
-            case '*':
-                result = Integer.toString(first * second);
-
-                break;
-            default:
-                result = "Error";
-
-        }
-
-
-        return result;
+        return switch (operator) {
+            case '+' -> Integer.toString(first + second);
+            case '-' -> Integer.toString(first - second);
+            case '*' -> Integer.toString(first * second);
+            default -> "Error";
+        };
     }
 
 }
