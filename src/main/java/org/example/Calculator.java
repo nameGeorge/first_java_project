61b25126ee64ed215436c0b77d4469586665ea2b
nameGeorge <geomel@yandex.ru>
2024-06-20 20:28:03 +0300

    рефакторинг

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,30 +1,27 @@
 package org.example;
 
 public class Calculator {
+    private static final char PLUS = '+';
+    private static final char MINUS = '-';
+    private static final char MULTIPLY = '*';
+
     public static void runCalculator() {
-
-        int first = Input.scan();
-
-        char operator = Input.readChar();
-
-        int second = Input.scan();
+        int first = Input.scanInt();
+        char operator = Input.scanChar();
+        int second = Input.scanInt();
 
         switch (operator) {
-            case '+':
+            case PLUS:
                 System.out.println(first + second);
                 break;
-            case '-':
+            case MINUS:
                 System.out.println(first - second);
                 break;
-            case '*':
+            case MULTIPLY:
                 System.out.println(first * second);
                 break;
             default:
                 System.out.println("Error");
         }
     }
-
 }
-
-
-
--- src/main/java/org/example/Input.java
+++ src/main/java/org/example/Input.java
@@ -3,12 +3,12 @@ package org.example;
 import java.util.Scanner;
 
 public class Input {
-    public static int scan() {
-
+    public static int scanInt() {
         Scanner sc = new Scanner(System.in);
         return sc.nextInt();
     }
-    public static char readChar() {
+
+    public static char scanChar() {
         Scanner sc = new Scanner(System.in);
         return sc.nextLine().charAt(0);
     }
